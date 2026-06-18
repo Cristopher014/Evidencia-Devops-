0becc79618384832c2b4b779f1e626748fd019f0
Cristopher014 <csonic049@gmail.com>
2026-06-18 14:11:36 -0500

    aviso de mantenimiento

--- src/App.js
+++ src/App.js
@@ -1,11 +1,6 @@
 import './App.css';
 
 function App() {
-  return (
-    <div>
-      <h1>Hola Mundo CineApp</h1>
-    </div>
-  );
+  return <h1>🚧 En mantenimiento 🚧</h1>;
 }
-
 export default App;
