68789efd341595f5ac94cafcc4a2b0c30126d205
Cristopher014 <csonic049@gmail.com>
2026-06-18 14:09:47 -0500

    Agregue mi renderizacion de  OMDb API

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
+import { useEffect, useState } from "react";
 import './App.css';
 
 function App() {
+  const [peliculas, setPeliculas] = useState([]);
+
+   <div>
+      <h1>Hola Mundo CineApp</h1>
+    </div>
+  useEffect(() => {
+    // Cambia TU_API_KEY por tu clave de OMDb
+    fetch("https://www.omdbapi.com/?s=batman&apikey=99b2be8")
+      .then(res => res.json())
+      .then(data => {
+        if (data.Search) {
+          setPeliculas(data.Search);
+        }
+      });
+  }, []);
+
   return (
     <div>
-      <h1>Hola Mundo CineApp</h1>
+      <h1>Lista de Películas</h1>
+      <ul>
+        {peliculas.map(p => (
+          <li key={p.imdbID}>{p.Title} ({p.Year})</li>
+        ))}
+      </ul>
     </div>
   );
 }
 
 export default App;
+
